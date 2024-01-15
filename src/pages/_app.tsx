8a794c564f540c8e864e811de939b889885062b9
MateusJSouza <mateusjdev@gmail.com>
2024-01-15 10:48:10 -0300

    feat: config stitches, globalStyles and header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+
+import { Container, Header } from '@/styles/pages/app'
+
+import logoImg from '../assets/logo.svg'
+import { globalStyles } from '../styles/global'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="Logo do ignite shop" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
